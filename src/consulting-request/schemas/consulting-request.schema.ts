dd6a367eb09143ae783b9fd5b4bd7509668a41fc
김혜린 <mye785c83@gmail.com>
2022-11-16 13:53:59 +0900

    :bug: schema validation, example 오류 수정

--- src/consulting-request/schemas/consulting-request.schema.ts
+++ src/consulting-request/schemas/consulting-request.schema.ts
@@ -13,12 +13,30 @@ export type ConsultingRequestDocument = ConsultingRequest & Document;
 export class ConsultingRequest {
     @IsObject()
     @Prop({ required: true })
+    @ApiProperty({example: {name: '아무개', sex: 'm', age: 19, phone: '01011112222'}})
     user: User;
     @IsObject()
     @Prop({ required: true })
+    @ApiProperty({example: {
+        korean: 89,
+        english: 89,
+        math: 89,
+        society: 89,
+        history: 89,
+        optional: 89,
+        average: 89
+    }})
     score: Score;
     @IsObject()
     @Prop({ required: true })
+    @ApiProperty({example: {
+        1: {university: '서강대학교', major: '컴퓨터공학과'},
+        2: {university: '서강대학교', major: '컴퓨터공학과'},
+        3: {university: '서강대학교', major: '컴퓨터공학과'},
+        4: {university: '미정', major: '미정'},
+        5: {university: '미정', major: '미정'},
+        6: {university: '미정', major: '미정'},
+        reason: '희망 대학/학과 지원 이유'}})
     uni: DesiredUni;
     @IsString()
     @Prop({required: true})
@@ -40,7 +58,7 @@ export class ConsultingRequest {
     @Prop({required: true})
     @ApiProperty({example: '참고사항'})
     reference: string;
-    @IsObject()
+    @IsArray()
     @Prop({required: true})
     @ApiProperty({type: Array<AdditionalInfoForm>, example: [{header: '2023 수능 응시자 필수 답변', title: '모의고사 등급을 작성해주세요.', value: '국어 1 영어 1 수학 1 한국사 1 사탐(세계지리 1 사회문화 1) 아랍어 1'}]})
     additionalInfo: AdditionalInfoForm;
