077d5d1dce3668a208905cbfbd11c2be62e6cbf1
김혜린 <mye785c83@gmail.com>
2023-04-11 12:20:23 +0900

    :art: error 컨트롤 방식 HttpException으로 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 
@@ -10,11 +10,21 @@ export class AuthService {
     ){}
 
     async signIn(email: string, password: string): Promise<string> {
-        const user = await this.userService.findOne(email);
-        if (user.password !== password) {
-            throw new UnauthorizedException(`Wrong Password`);
+        try{
+            const user = await this.userService.findOne(email);
+            if (user.password !== password) {
+                throw new HttpException(
+                    "잘못된 사용자 정보입니다.",
+                    HttpStatus.BAD_REQUEST
+                );
+            }
+            const payload = { username: user.username, sub: user.email };
+            return await this.jwtService.signAsync(payload);
+        }catch(err) {
+            throw new HttpException(
+                '서버 오류가 발생했습니다.',
+                HttpStatus.INTERNAL_SERVER_ERROR
+            )
         }
-        const payload = { username: user.username, sub: user.email };
-        return await this.jwtService.signAsync(payload);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.schema';
@@ -12,7 +12,10 @@ export class UserService {
     async create(user: User): Promise<User> {
         const existingUser = await this.userModel.find({email: user.email}).exec();
         if(existingUser.length !== 0) {
-            throw new ConflictException(`User #${user.email} already existed`);
+            throw new HttpException(
+                '이미 존재하는 이메일입니다.',
+                HttpStatus.BAD_REQUEST
+            );
         }
         return await new this.userModel(user).save();
     }
@@ -20,7 +23,10 @@ export class UserService {
     async findOne(email: string): Promise<User> {
         const user =  await this.userModel.findOne({email: email}).exec();
         if(!user) {
-            throw new NotFoundException(`User #${email} not found`);
+            throw new HttpException(
+                '잘못된 사용자 정보입니다.',
+                HttpStatus.BAD_REQUEST
+            );
         }
         return user;
     }   
